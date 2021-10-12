f7ea373b7e5b41aa73a5c61cbf8b52ffec729a43
Steve Gomez <StevenAGomez@Live.com>
2021-10-12 12:12:46 -0700

    Adds home page and route

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,49 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
 import './App.css';
 
 const Application1Module1 = React.lazy(() => import('Application1/Module1'));
 
 const App = () => {
   return (
-    <div
-      style={{
-        minHeight: '100vh',
-        width: '100%',
-        display: 'flex',
-        boxSizing: 'border-box',
-      }}
-    >
-      <div style={{ flexBasis: '200px', backgroundColor: '#bada55' }}>Menu</div>
-      <React.Suspense fallback="Loading Button">
-        <Application1Module1 name="Steve" />
-      </React.Suspense>
-    </div>
+    <Router>
+      <div
+        style={{
+          minHeight: '100vh',
+          width: '100%',
+          display: 'flex',
+          boxSizing: 'border-box',
+        }}
+      >
+        <div style={{ flexBasis: '200px', backgroundColor: '#bada55' }}>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/module1">Module 1</Link>
+              </li>
+              <li>
+                <Link to="/module2">Module 2</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Switch>
+          <Route path="/module1">
+            <React.Suspense fallback="Loading Module 1">
+              <Application1Module1 name="via Container" />
+            </React.Suspense>
+          </Route>
+          <Route path="/module2">TODO</Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
