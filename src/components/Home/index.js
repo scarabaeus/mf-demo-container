9cd932b6a1ce4a80c0019e9c809eac38d459bc3d
Steve Gomez <StevenAGomez@Live.com>
2021-10-28 12:00:51 -0700

    Adds dependencies section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,20 @@ const Home = () => {
   return (
     <div>
       <h1>Container's Home Page</h1>
+      <div style={{ marginTop: '1rem' }}>
+        mf-demo-container Dependencies:
+        <ul>
+          <li>
+            <code>react 17.0.2</code>
+          </li>
+          <li>
+            <code>react-router 17.0.2</code>
+          </li>
+          <li>
+            <code>react-router-dom 5.3.0</code>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
